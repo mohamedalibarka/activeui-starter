fbfdba587dbeaec60807501933c76e092c462120
Pchol22 <pierre.chollet@student-cs.fr>
2022-01-25 19:15:32 +0100

    turn into NPM lib - MVP is working

---
+++ src/modulePlugins.tsx
@@ -0,0 +1,5 @@
+import { pluginWidgetMap } from './pluginWidgetMap';
+import { pluginWidgetSunburst } from './pluginWidgetSunburst';
+
+// Put newly created plugins here to add them to NPM library
+export { pluginWidgetMap, pluginWidgetSunburst };
