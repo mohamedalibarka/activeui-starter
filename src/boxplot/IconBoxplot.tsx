12602faf5b2cf8d34d53c7a9ff9060e661b089f3
Pchol22 <pierre.chollet@student-cs.fr>
2022-03-24 12:13:33 +0100

    rebase

--- src/boxplot/IconBoxplot.tsx
+++ src/boxplot/IconBoxplot.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-/**
- * Large icon of the earth, usable to represent the map in the widgets ribbon.
- */
 export const IconBoxplot = () => (
-    <svg height="32" width="44" viewBox="0 0 1024 1024">
+    <svg height="30" width="40" viewBox="0 0 100 100">
         <path
             xmlns="http://www.w3.org/2000/svg"
             fill="#d9d9d9"
-            d="M952 224h-52c-4.4 0-8 3.6-8 8v248h-92V304c0-4.4-3.6-8-8-8H232c-4.4 0-8 3.6-8 8v176h-92V232c0-4.4-3.6-8-8-8H72c-4.4 0-8 3.6-8 8v560c0 4.4 3.6 8 8 8h52c4.4 0 8-3.6 8-8V548h92v172c0 4.4 3.6 8 8 8h560c4.4 0 8-3.6 8-8V548h92v244c0 4.4 3.6 8 8 8h52c4.4 0 8-3.6 8-8V232c0-4.4-3.6-8-8-8zM296 368h88v288h-88V368zm432 288H448V368h280v288z"
+            d="M25.8 37.2h4v30.9H19.1v15.4h10.7v4H5v-4h10.1V68.1H5V37.2h20.8zM19.1 9h10.7V5H5v4h10.1v10.7H5v13.4h24.8V19.8H19.1V9zm23.5 57.8h-4v11.4h10.1v8.7H38.6v4h24.9v-4H52.7v-8.7h10.7V66.8H42.6zm20.8-39.6v-4H38.6v4h10.1v23.5H38.6v12.1h24.8V50.7H52.7V27.2h10.7zm10.8 23.5h-4v13.4h10.1V91H70.1v4H95v-4H84.3V64.1H95V50.7H74.2zM95 19.8v-4H70.1v4h10.1v5.4H70.1v21.5H95V25.1H84.3v-5.4H95z"
         />
     </svg>
 );
